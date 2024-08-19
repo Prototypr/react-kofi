cb90b8430c7060eb3782a3b48c374e91a79f0b4c
Graeme Fulton <graeme@prototypr.io>
2024-08-19 12:15:57 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './kofi.css';
 import KoFiDialog from "./KoFiDialog";
 import KoFiButton from "./KoFiButton";
 import KoFiWidget from "./KoFiWidget";
-import KoFiPanel from './KofiPanel';
+import KoFiPanel from './KoFiPanel';
 
 export { KoFiDialog, KoFiButton, KoFiWidget, KoFiPanel };
